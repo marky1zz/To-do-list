22c6231e362ad27a54be4cf974c38b5dcf565386
Martinas <mart1z5916@gmail.com>
2023-06-12 23:04:25 +0300

    added login classes and login.jsp file

--- src/main/java/com/mar/todoapp/DAO/LoginDAO.java
+++ src/main/java/com/mar/todoapp/DAO/LoginDAO.java
@@ -1,5 +1,38 @@
 package com.mar.todoapp.DAO;
 
-public class LoginDAO {
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+import com.mar.todoapp.model.LoginBean;
+import com.mar.todoapp.util.JDBCUtils;
+
+public class LoginDAO 
+{
+	public boolean validate(LoginBean loginBean) throws ClassNotFoundException
+	{
+		boolean status = false;
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		try (Connection connection = JDBCUtils.getConnection())
+		{
+			PreparedStatement preparedStatement = connection.prepareStatement("select * from users where username = ? and password = ? ");
+			preparedStatement.setString(1, loginBean.getUsername());
+			preparedStatement.setString(2, loginBean.getPassword());
+			
+			System.out.println(preparedStatement);
+		
+			ResultSet resultSet = preparedStatement.executeQuery();
+			status = resultSet.next();
+			
+		}
+		catch (SQLException e)
+		{
+			JDBCUtils.printSQLException(e);
+		}
+		
+		return status;
+	}
 }
--- src/main/java/com/mar/todoapp/web/LoginController.java
+++ src/main/java/com/mar/todoapp/web/LoginController.java
@@ -1,5 +1,66 @@
 package com.mar.todoapp.web;
 
-public class LoginController {
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
+import java.io.IOException;
+
+import com.mar.todoapp.DAO.LoginDAO;
+import com.mar.todoapp.model.LoginBean;
+
+@WebServlet("/login")
+public class LoginController extends HttpServlet
+{
+	private static final long serialVersionUID = 1L;
+	private LoginDAO loginDAO;
+	
+	public void init()
+	{
+		loginDAO = new LoginDAO();
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		response.sendRedirect("login/login.jsp");
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		authenticate(request, response);
+	}
+	
+	public void authenticate(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		LoginBean loginBean = new LoginBean();
+		
+		loginBean.setUsername(username);
+		loginBean.setPassword(password);
+		
+		try
+		{
+			if (loginDAO.validate(loginBean))
+			{
+				RequestDispatcher dispatcher = request.getRequestDispatcher("todo/todo-list.jsp");
+				dispatcher.forward(request, response);
+			}
+			else
+			{
+				HttpSession session = request.getSession();
+				// session.setAttribute("user", username);
+				response.sendRedirect("login/login.jsp");
+			}
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}		
+	}	
 }
