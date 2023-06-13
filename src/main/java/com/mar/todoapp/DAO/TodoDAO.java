a0c1bc876581c2a418b96599c340fb0157f67341
Martinas <mart1z5916@gmail.com>
2023-06-13 22:40:58 +0300

    added TodoDAO interface and Todo class

--- src/main/java/com/mar/todoapp/DAO/TodoDAO.java
+++ src/main/java/com/mar/todoapp/DAO/TodoDAO.java
@@ -1,5 +1,19 @@
 package com.mar.todoapp.DAO;
 
-public class TodoDAO {
+import java.sql.SQLException;
+import java.util.List;
 
+import com.mar.todoapp.model.Todo;
+
+public interface TodoDAO 
+{
+	void insertTodo(Todo todo) throws SQLException;
+	
+	Todo selectTodo(long todoId);
+	
+	List<Todo> selectAllTodos();
+	
+	boolean deleteTodo(int id) throws SQLException;
+	
+	boolean updateTodo(Todo todo) throws SQLException;
 }
--- src/main/java/com/mar/todoapp/model/Todo.java
+++ src/main/java/com/mar/todoapp/model/Todo.java
@@ -1,5 +1,126 @@
 package com.mar.todoapp.model;
 
-public class Todo {
+import java.time.LocalDate;
 
+public class Todo 
+{
+	private long id;
+	private String title;
+	private String username;
+	private String description;
+	private LocalDate targetDate;
+	private boolean status;
+	
+	protected Todo()
+	{
+		
+	}
+	
+	public Todo(long id, String title, String username, String description, LocalDate targetDate, boolean isDone) 
+	{
+		super();
+		this.id = id;
+		this.title = title;
+		this.username = username;
+		this.description = description;
+		this.targetDate = targetDate;
+		this.status = isDone;
+	}
+
+	public Todo(String title, String username, String description, LocalDate targetDate, boolean isDone) 
+	{
+		super();
+		this.title = title;
+		this.username = username;
+		this.description = description;
+		this.targetDate = targetDate;
+		this.status = isDone;
+	}
+
+	public long getId() 
+	{
+		return id;
+	}
+	
+	public void setId(long id) 
+	{
+		this.id = id;
+	}
+	
+	public String getTitle() 
+	{
+		return title;
+	}
+	
+	public void setTitle(String title) 
+	{
+		this.title = title;
+	}
+	
+	public String getUsername() 
+	{
+		return username;
+	}
+	
+	public void setUsername(String username) 
+	{
+		this.username = username;
+	}
+	
+	public String getDescription() 
+	{
+		return description;
+	}
+	
+	public void setDescription(String description) 
+	{
+		this.description = description;
+	}
+	
+	public LocalDate getTargetDate() 
+	{
+		return targetDate;
+	}
+	
+	public void setTargetDate(LocalDate targetDate) 
+	{
+		this.targetDate = targetDate;
+	}
+	
+	public boolean isStatus() 
+	{
+		return status;
+	}
+	
+	public void setStatus(boolean status) 
+	{
+		this.status = status;
+	}
+	
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		
+		result = prime * result + (int)(id ^ (id >>> 32));
+		
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj)
+	{
+		Todo other = (Todo) obj;
+		
+		if (this == obj) return true;
+		
+		if (obj == null) return false;
+		
+		if (getClass() != obj.getClass()) return false;
+		
+		if (id != other.id) return false;
+		
+		return true;
+	}	
 }
